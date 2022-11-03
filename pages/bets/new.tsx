da00a2d51078be070bcce069cfd94445da6ad701
kiv1n <aloha@kiv1n.ru>
2022-11-04 00:58:15 +0600

    improve style for new bet page, update global button styles

--- pages/bets/new.tsx
+++ pages/bets/new.tsx
@@ -1,15 +1,12 @@
 import { LoadingButton } from "@mui/lab";
 import {
   Box,
-  FormControl,
-  InputLabel,
-  Link as MuiLink,
+  Button,
   MenuItem,
   Select,
   TextField,
   Typography,
 } from "@mui/material";
-import { Stack } from "@mui/system";
 import FormikHelper from "components/helpers/FormikHelper";
 import Layout from "components/layout/Layout";
 import { betContractAbi } from "contracts/abi/betContract";
@@ -17,11 +14,12 @@ import { BigNumber, ethers } from "ethers";
 import { Form, Formik } from "formik";
 import useDebounce from "hooks/useDebounce";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   useAccount,
   useContractEvent,
   useContractWrite,
+  useNetwork,
   usePrepareContractWrite,
   useWaitForTransaction,
 } from "wagmi";
@@ -31,6 +29,47 @@ import * as yup from "yup";
  * Page to make a new bet.
  */
 export default function NewBet() {
+  const [createdBetId, setCreatedBetId] = useState<string | undefined>();
+
+  return (
+    <Layout>
+      {createdBetId ? (
+        <CreatedBetMessage betId={createdBetId} />
+      ) : (
+        <CreateBetForm
+          onSuccessCreate={(createdBetId) => setCreatedBetId(createdBetId)}
+        />
+      )}
+    </Layout>
+  );
+}
+
+function CreatedBetMessage(props: { betId: string }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        mt: 4,
+      }}
+    >
+      <Typography variant="h4" fontWeight={700} sx={{ mb: 3 }}>
+        🤞 Congrats, your bet is published!
+      </Typography>
+      <Link href={`/bets/${props.betId}`} legacyBehavior passHref>
+        <Button
+          variant="contained"
+          sx={{ borderRadius: 32, fontSize: 18, px: 4, py: 2 }}
+        >
+          Open
+        </Button>
+      </Link>
+    </Box>
+  );
+}
+
+function CreateBetForm(props: { onSuccessCreate: (betId: string) => void }) {
   // Form states
   const [formValues, setFormValues] = useState({
     rate: 0.01,
@@ -50,6 +89,8 @@ export default function NewBet() {
 
   // Account state
   const { address } = useAccount();
+  // Network state
+  const { chain } = useNetwork();
   // Contract states
   const { config: contractConfig } = usePrepareContractWrite({
     address: process.env.NEXT_PUBLIC_BET_CONTRACT_ADDRESS,
@@ -66,7 +107,6 @@ export default function NewBet() {
     useWaitForTransaction({
       hash: contractWriteData?.hash,
     });
-  const [madeBetId, setMadeBetId] = useState<string | undefined>();
 
   const isFormDisabled =
     isContractWriteLoading || isTransactionLoading || isTransactionSuccess;
@@ -98,118 +138,237 @@ export default function NewBet() {
     eventName: "Transfer",
     listener(from, to, tokenId) {
       if (from === ethers.constants.AddressZero && to === address) {
-        setMadeBetId(tokenId.toString());
+        props.onSuccessCreate(tokenId.toString());
       }
     },
   });
 
   return (
-    <Layout>
-      {/* Intro message */}
-      <Typography variant="h4" fontWeight={700} gutterBottom>
-        Dear Web3,
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        mt: 4,
+      }}
+    >
+      <Typography
+        variant="h4"
+        fontWeight={700}
+        textAlign="center"
+        gutterBottom
+        sx={{ mb: 3 }}
+      >
+        🙏 Dear Web3,
       </Typography>
-      {/* Form */}
-      <Box sx={{ mt: 4 }}>
-        <Formik
-          initialValues={formValues}
-          validationSchema={formValidationSchema}
-          onSubmit={() => contractWrite?.()}
-        >
-          {({ values, errors, touched, handleChange }) => (
-            <Form>
-              <FormikHelper onChange={(values: any) => setFormValues(values)} />
-              <Stack direction="column" spacing={2}>
-                <TextField
-                  fullWidth
-                  id="rate"
-                  name="rate"
-                  label="I bet"
-                  type="number"
-                  value={values.rate}
-                  onChange={handleChange}
-                  error={touched.rate && Boolean(errors.rate)}
-                  helperText={touched.rate && errors.rate}
-                  disabled={isFormDisabled}
-                />
-                <FormControl fullWidth>
-                  <InputLabel id="symbol-label">That</InputLabel>
-                  <Select
-                    labelId="symbol-label"
-                    id="symbol"
-                    name="symbol"
-                    label="That"
-                    value={values.symbol}
-                    onChange={handleChange}
-                    disabled={isFormDisabled}
-                  >
-                    <MenuItem value="ETHUSD">ETHUSD</MenuItem>
-                    <MenuItem value="BTCUSD">BTCUSD</MenuItem>
-                  </Select>
-                </FormControl>
-                <Typography fontWeight={700}>will be cost</Typography>
-                <TextField
-                  fullWidth
-                  id="minPrice"
-                  name="minPrice"
-                  label="More than"
-                  type="number"
-                  value={values.minPrice}
-                  onChange={handleChange}
-                  error={touched.minPrice && Boolean(errors.minPrice)}
-                  helperText={touched.minPrice && errors.minPrice}
-                  disabled={isFormDisabled}
-                />
-                <Typography fontWeight={700}>and</Typography>
-                <TextField
-                  fullWidth
-                  id="maxPrice"
-                  name="maxPrice"
-                  label="Less than"
-                  type="number"
-                  value={values.maxPrice}
-                  onChange={handleChange}
-                  error={touched.maxPrice && Boolean(errors.maxPrice)}
-                  helperText={touched.maxPrice && errors.maxPrice}
-                  disabled={isFormDisabled}
-                />
-                <TextField
-                  fullWidth
-                  id="date"
-                  name="date"
-                  label="On"
-                  type="date"
-                  value={values.date}
-                  onChange={handleChange}
-                  error={touched.date && Boolean(errors.date)}
-                  helperText={touched.date && errors.date}
-                  disabled={isFormDisabled}
-                />
-                <LoadingButton
-                  loading={isContractWriteLoading || isTransactionLoading}
-                  variant="contained"
-                  fullWidth
-                  type="submit"
-                  disabled={isFormDisabled || !contractWrite}
-                >
-                  Make Bet
-                </LoadingButton>
-              </Stack>
-            </Form>
-          )}
-        </Formik>
-      </Box>
-      {/* Success message */}
-      {madeBetId && (
-        <Box sx={{ mt: 4 }}>
-          <Typography variant="h4" fontWeight={700} gutterBottom>
-            Bet is made!
-          </Typography>
-          <Link href={`/bets/${madeBetId}`} legacyBehavior passHref>
-            <MuiLink>Link</MuiLink>
-          </Link>
-        </Box>
-      )}
-    </Layout>
+      <Formik
+        initialValues={formValues}
+        validationSchema={formValidationSchema}
+        onSubmit={() => contractWrite?.()}
+      >
+        {({ values, errors, touched, handleChange }) => (
+          <Form>
+            <FormikHelper onChange={(values: any) => setFormValues(values)} />
+            {/* Rate input */}
+            <CreateBetInputBox
+              title="I bet"
+              subtitle={chain?.nativeCurrency?.symbol}
+              color="#2B6EFD"
+              sx={{ mb: 2 }}
+            >
+              <TextField
+                id="rate"
+                name="rate"
+                type="number"
+                value={values.rate}
+                onChange={handleChange}
+                error={touched.rate && Boolean(errors.rate)}
+                helperText={touched.rate && errors.rate}
+                disabled={isFormDisabled}
+                variant="standard"
+                sx={{
+                  width: 120,
+                  bgcolor: "#FFFFFF",
+                  borderRadius: 4,
+                  py: 1,
+                  px: 2,
+                }}
+              />
+            </CreateBetInputBox>
+            {/* Symbol input */}
+            <CreateBetInputBox title="That" color="#333333" sx={{ mb: 3 }}>
+              <Select
+                labelId="symbol-label"
+                id="symbol"
+                name="symbol"
+                value={values.symbol}
+                onChange={handleChange}
+                disabled={isFormDisabled}
+                variant="standard"
+                sx={{
+                  width: 160,
+                  bgcolor: "#FFFFFF",
+                  borderRadius: 4,
+                  py: 1,
+                  px: 2,
+                }}
+              >
+                <MenuItem value="ETHUSD">ETH</MenuItem>
+                <MenuItem value="BTCUSD">BTC</MenuItem>
+              </Select>
+            </CreateBetInputBox>
+            {/* Text divider */}
+            <Typography fontWeight={700} textAlign="center" sx={{ mb: 3 }}>
+              will be cost
+            </Typography>
+            {/* Min price input */}
+            <CreateBetInputBox
+              title="More than"
+              subtitle="USD"
+              color="#1DB954"
+              sx={{ mb: 3 }}
+            >
+              <TextField
+                id="minPrice"
+                name="minPrice"
+                type="number"
+                value={values.minPrice}
+                onChange={handleChange}
+                error={touched.minPrice && Boolean(errors.minPrice)}
+                helperText={touched.minPrice && errors.minPrice}
+                disabled={isFormDisabled}
+                variant="standard"
+                sx={{
+                  width: 120,
+                  bgcolor: "#FFFFFF",
+                  borderRadius: 4,
+                  py: 1,
+                  px: 2,
+                }}
+              />
+            </CreateBetInputBox>
+            {/* Text divider */}
+            <Typography fontWeight={700} textAlign="center" sx={{ mb: 3 }}>
+              and
+            </Typography>
+            {/* Min price input */}
+            <CreateBetInputBox
+              title="Less than"
+              subtitle="USD"
+              color="#FF4400"
+              sx={{ mb: 2 }}
+            >
+              <TextField
+                id="maxPrice"
+                name="maxPrice"
+                type="number"
+                value={values.maxPrice}
+                onChange={handleChange}
+                error={touched.maxPrice && Boolean(errors.maxPrice)}
+                helperText={touched.maxPrice && errors.maxPrice}
+                disabled={isFormDisabled}
+                variant="standard"
+                sx={{
+                  width: 120,
+                  bgcolor: "#FFFFFF",
+                  borderRadius: 4,
+                  py: 1,
+                  px: 2,
+                }}
+              />
+            </CreateBetInputBox>
+            {/* Date */}
+            <CreateBetInputBox title="On" color="#4B144B" sx={{ mb: 6 }}>
+              <TextField
+                id="date"
+                name="date"
+                type="date"
+                value={values.date}
+                onChange={handleChange}
+                error={touched.date && Boolean(errors.date)}
+                helperText={touched.date && errors.date}
+                disabled={isFormDisabled}
+                variant="standard"
+                sx={{
+                  width: 180,
+                  bgcolor: "#FFFFFF",
+                  borderRadius: 4,
+                  py: 1,
+                  px: 2,
+                }}
+              />
+            </CreateBetInputBox>
+            {/* Submit button */}
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                mb: 4,
+              }}
+            >
+              <LoadingButton
+                loading={isContractWriteLoading || isTransactionLoading}
+                variant="contained"
+                type="submit"
+                disabled={isFormDisabled || !contractWrite}
+                sx={{
+                  borderRadius: 32,
+                  fontSize: 24,
+                  px: 5,
+                  py: 2.5,
+                }}
+              >
+                Make Bet
+              </LoadingButton>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Box>
+  );
+}
+
+function CreateBetInputBox(props: {
+  title: string;
+  subtitle?: string;
+  color?: string;
+  sx?: any;
+  children: any;
+}) {
+  const [subtitle, setSubtitle] = useState<string | undefined>();
+
+  useEffect(() => {
+    setSubtitle(props.subtitle);
+  }, [props.subtitle]);
+
+  return (
+    <Box
+      sx={{
+        width: 540,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        bgcolor: props.color || "#000000",
+        py: 2,
+        px: 4,
+        borderRadius: 3,
+        ...props.sx,
+      }}
+    >
+      <Typography
+        sx={{
+          color: "#FFFFFF",
+          fontSize: 32,
+          fontWeight: 700,
+          minWidth: 180,
+          mr: 3,
+        }}
+      >
+        {props.title}
+      </Typography>
+      {props.children}
+      <Typography sx={{ color: "#FFFFFF", ml: 2 }}>{subtitle}</Typography>
+    </Box>
   );
 }
--- theme/components.ts
+++ theme/components.ts
@@ -5,8 +5,27 @@ export const components: Components = {
     styleOverrides: {
       root: {
         textTransform: "initial",
+        boxShadow: "none",
+        "&:hover": {
+          boxShadow: "none",
+        },
       },
     },
+    variants: [
+      {
+        props: { variant: "contained" },
+        style: {
+          color: "#FFFFFF",
+          background: "#000000",
+          "& .MuiButton-startIcon svg path": {
+            stroke: "#FFFFFF",
+          },
+          "&:hover": {
+            background: "#433d39",
+          },
+        },
+      },
+    ],
   },
   MuiLink: {
     styleOverrides: {
