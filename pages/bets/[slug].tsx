2c8d0d4bd03e7b960280fccd56c906fd0b2316c6
kiv1n <aloha@kiv1n.ru>
2022-11-03 14:18:37 +0600

    Add button to accept bet

--- pages/bets/[slug].tsx
+++ pages/bets/[slug].tsx
@@ -1,10 +1,18 @@
-import { Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Stack, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import Layout from "components/layout/Layout";
 import { betContractAbi } from "contracts/abi/betContract";
 import { BigNumber, ethers } from "ethers";
 import { useRouter } from "next/router";
-import { useContractRead } from "wagmi";
+import { useEffect } from "react";
+import {
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
 
 /**
  * Page with a bet.
@@ -13,23 +21,20 @@ export default function Bet() {
   const router = useRouter();
   const { slug } = router.query;
 
-  // Download bet params
-  // Show bet params
-
   return (
     <Layout>{slug && <BetParams betId={slug as string}></BetParams>}</Layout>
   );
 }
 
 function BetParams(props: { betId: string }) {
-  const { data, isError, isLoading, isSuccess } = useContractRead({
+  const { data, isSuccess, refetch, isFetching } = useContractRead({
     address: process.env.NEXT_PUBLIC_BET_CONTRACT_ADDRESS,
     abi: betContractAbi,
     functionName: "getParams",
     args: [BigNumber.from(props.betId)],
   });
 
-  if (isSuccess && data) {
+  if (!isFetching && isSuccess && data) {
     return (
       <Box>
         <Typography variant="h4" fontWeight={700} gutterBottom>
@@ -55,9 +60,90 @@ function BetParams(props: { betId: string }) {
           secondMember: {data.secondMember.toString()}
         </Typography>
         <Typography gutterBottom>winner: {data.winner.toString()}</Typography>
+        {/* Buttons */}
+        <Stack sx={{ mt: 3 }} direction="row" spacing={1}>
+          <BetAcceptButton
+            betId={BigNumber.from(props.betId)}
+            betRate={data.rate}
+            betFirstMember={data.firstMember}
+            betSecondMember={data.secondMember}
+            onSuccess={() => refetch()}
+          />
+          <BetVerifyButton />
+          <LoadingButton
+            variant="outlined"
+            onClick={() => {
+              refetch();
+            }}
+          >
+            Refetch
+          </LoadingButton>
+        </Stack>
       </Box>
     );
   }
 
   return <></>;
+}
+
+function BetAcceptButton(props: {
+  betId: BigNumber;
+  betRate: BigNumber;
+  betFirstMember: string;
+  betSecondMember: string;
+  // betWinner: string,
+  onSuccess: Function;
+}) {
+  // Account state
+  const { address } = useAccount();
+  // Contract states
+  const contractArgs: any = [props.betId, { value: props.betRate }];
+  const { config: contractConfig } = usePrepareContractWrite({
+    address: process.env.NEXT_PUBLIC_BET_CONTRACT_ADDRESS,
+    abi: betContractAbi,
+    functionName: "accept",
+    args: contractArgs,
+  });
+  const {
+    data: contractWriteData,
+    isLoading: isContractWriteLoading,
+    write: contractWrite,
+  } = useContractWrite(contractConfig);
+  const { isLoading: isTransactionLoading, isSuccess: isTransactionSuccess } =
+    useWaitForTransaction({
+      hash: contractWriteData?.hash,
+    });
+
+  // TODO: Disable if winner is not empty
+  const isDisabled =
+    !contractWrite ||
+    props.betFirstMember === address ||
+    props.betSecondMember !== ethers.constants.AddressZero;
+
+  useEffect(() => {
+    if (isTransactionSuccess) {
+      // TODO: Show alert with success message
+      props.onSuccess();
+    }
+  }, [isTransactionSuccess]);
+
+  return (
+    <LoadingButton
+      variant="contained"
+      disabled={isDisabled}
+      loading={isContractWriteLoading || isTransactionLoading}
+      onClick={() => contractWrite?.()}
+    >
+      Accept
+    </LoadingButton>
+  );
+}
+
+// TODO: Implement button
+function BetVerifyButton() {
+  return (
+    <LoadingButton variant="contained" disabled={true}>
+      Verify
+    </LoadingButton>
+  );
 }
