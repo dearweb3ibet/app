ac7bc33649436141c39607be447856a3197ca9aa
kiv1n <aloha@kiv1n.ru>
2022-12-17 14:43:55 +0300

    Add button to load more participations on account page

--- components/account/AccountBetParticipationList.tsx
+++ components/account/AccountBetParticipationList.tsx
@@ -1,14 +1,13 @@
 import { Skeleton, SxProps } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import BetCard from "components/bet/BetCard";
+import { XxlLoadingButton } from "components/styled";
 import useError from "hooks/useError";
 import useSubgraph from "hooks/useSubgraph";
 import { useEffect, useState } from "react";
 
 /**
  * A component with account bet participations.
- *
- * TODO: Add button load more
  */
 export default function AccountBetParticipationList(props: {
   address: string;
@@ -18,43 +17,82 @@ export default function AccountBetParticipationList(props: {
 }) {
   const { handleError } = useError();
   const { findBetParticipants } = useSubgraph();
-  const [betParticipants, setBetParticipants] = useState<[] | undefined>();
+  const [participants, setParticipants] = useState<any[] | undefined>();
+  const [isMoreParticipantsExist, setIsMoreParticipantsExist] = useState(true);
+  const [pageNumber, setPageNumber] = useState(0);
+  const pageSize = 1;
+
+  async function loadMoreParticipants() {
+    try {
+      const nextPageNumber = pageNumber + 1;
+      const loadedParticipants = await findBetParticipants(
+        props.address,
+        props.isCreator,
+        props.isFeeForSuccess,
+        pageSize,
+        (nextPageNumber - 1) * pageSize
+      );
+      setParticipants(
+        participants
+          ? [...participants, ...loadedParticipants]
+          : loadedParticipants
+      );
+      setPageNumber(nextPageNumber);
+      if (loadedParticipants.length === 0) {
+        setIsMoreParticipantsExist(false);
+      }
+    } catch (error: any) {
+      handleError(error, true);
+    }
+  }
 
   useEffect(() => {
-    // Clear bet participants
-    setBetParticipants(undefined);
-    // Load bet participants
-    findBetParticipants(
-      props.address,
-      props.isCreator,
-      props.isFeeForSuccess,
-      3,
-      0
-    )
-      .then((result) => setBetParticipants(result))
-      .catch((error) => handleError(error, true));
+    setParticipants(undefined);
+    loadMoreParticipants();
   }, [props.address, props.isCreator, props.isFeeForSuccess]);
 
   return (
     <Box sx={{ ...props.sx }}>
-      {betParticipants ? (
-        <Stack spacing={2}>
-          {betParticipants.map((participant: any) => (
-            <BetCard
-              id={participant.bet.id}
-              creatorAddress={participant.bet.params.creatorAddress}
-              symbol={participant.bet.params.symbol}
-              targetMinPrice={participant.bet.params.targetMinPrice}
-              targetMaxPrice={participant.bet.params.targetMaxPrice}
-              targetTimestamp={participant.bet.params.targetTimestamp}
-              feeForSuccess={participant.bet.params.feeForSuccess}
-              feeForFailure={participant.bet.params.feeForFailure}
-              isClosed={participant.bet.params.isClosed}
-              isSuccessful={participant.bet.params.isSuccessful}
-              participantsNumber={participant.bet.participantsNumber}
-            />
-          ))}
-        </Stack>
+      {participants ? (
+        <>
+          {/* List */}
+          <Stack spacing={2}>
+            {participants.map((participant: any) => (
+              <BetCard
+                key={participant.id}
+                id={participant.bet.id}
+                creatorAddress={participant.bet.params.creatorAddress}
+                symbol={participant.bet.params.symbol}
+                targetMinPrice={participant.bet.params.targetMinPrice}
+                targetMaxPrice={participant.bet.params.targetMaxPrice}
+                targetTimestamp={participant.bet.params.targetTimestamp}
+                feeForSuccess={participant.bet.params.feeForSuccess}
+                feeForFailure={participant.bet.params.feeForFailure}
+                isClosed={participant.bet.params.isClosed}
+                isSuccessful={participant.bet.params.isSuccessful}
+                participantsNumber={participant.bet.participantsNumber}
+              />
+            ))}
+          </Stack>
+          {/* Actions */}
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+            sx={{ mt: 4 }}
+          >
+            {isMoreParticipantsExist && (
+              <XxlLoadingButton
+                variant="outlined"
+                onClick={() => {
+                  loadMoreParticipants();
+                }}
+              >
+                Load More
+              </XxlLoadingButton>
+            )}
+          </Stack>
+        </>
       ) : (
         <Skeleton variant="rounded" sx={{ width: "100%", height: 48 }} />
       )}
