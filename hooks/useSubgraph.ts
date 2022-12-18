7304117e951a759b25dce85de7b4697db9a79baa
kiv1n <aloha@kiv1n.ru>
2022-12-18 16:35:06 +0300

    Refactor subgraph functions

--- hooks/useSubgraph.ts
+++ hooks/useSubgraph.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 /**
  * Hook to work with subgraph.
+ *
+ * TODO: Use types for values, that functions return
  */
 export default function useSubgraph() {
   // TODO: Make next function relevant to current subgraph structure
@@ -10,9 +12,31 @@ export default function useSubgraph() {
     first?: number,
     skip?: number
   ): Promise<Array<any>> {
-    const response = await makeSubgraphQuery(
-      getFindBetsQuery(firstMember, first, skip)
-    );
+    // Filters and params
+    const firstMemberFilter = firstMember
+      ? `firstMember: "${firstMember.toLowerCase()}"`
+      : "";
+    const filterParams = `where: {${firstMemberFilter}}`;
+    const sortParams = `orderBy: createdDate, orderDirection: desc`;
+    const paginationParams = `first: ${first || 10}, skip: ${skip || 0}`;
+    // Query
+    const query = `{
+      bets(${filterParams}, ${sortParams}, ${paginationParams}) {
+        id
+        createdDate
+        symbol
+        minPrice
+        maxPrice
+        dayStartTimestamp
+        rate
+        firstMember
+        secondMember
+        winner
+        winning
+      }
+    }`;
+    // Make query and return result
+    const response = await makeSubgraphQuery(query);
     return response.bets;
   };
 
@@ -23,15 +47,40 @@ export default function useSubgraph() {
     first?: number,
     skip?: number
   ): Promise<Array<any>> {
-    const response = await makeSubgraphQuery(
-      getFindBetParticipantsQuery(
-        accountAddress,
-        isCreator,
-        isFeeForSuccess,
-        first,
-        skip
-      )
-    );
+    // Filters and params
+    const accountAddressFilter = accountAddress
+      ? `accountAddress: "${accountAddress.toLowerCase()}"`
+      : "";
+    const isCreatorFilter =
+      isCreator !== undefined ? `isCreator: ${isCreator}` : "";
+    const isFeeForSuccessFilter =
+      isFeeForSuccess !== undefined
+        ? `isFeeForSuccess: ${isFeeForSuccess}`
+        : "";
+    const filterParams = `where: {${accountAddressFilter}, ${isCreatorFilter}, ${isFeeForSuccessFilter}}`;
+    const sortParams = `orderBy: addedTimestamp, orderDirection: desc`;
+    const paginationParams = `first: ${first || 10}, skip: ${skip || 0}`;
+    // Query
+    const query = `{
+      betParticipants(${filterParams}, ${sortParams}, ${paginationParams}) {
+        id
+        bet {
+          id
+          creatorAddress
+          symbol
+          targetMinPrice
+          targetMaxPrice
+          targetTimestamp
+          feeForSuccess
+          feeForFailure
+          isClosed
+          isSuccessful
+          participantsNumber
+        }
+      }
+    }`;
+    // Make query and return result
+    const response = await makeSubgraphQuery(query);
     return response.betParticipants;
   };
 
@@ -41,14 +90,23 @@ export default function useSubgraph() {
     first?: number,
     skip?: number
   ): Promise<Array<any>> {
-    const response = await makeSubgraphQuery(
-      getFindContestWaveParticipantsQuery(
-        contestAddress,
-        waveIndex,
-        first,
-        skip
-      )
-    );
+    // Filters and params
+    const waveFilter = `wave: "${contestAddress.toLowerCase()}_${waveIndex}"`;
+    const filterParams = `where: {${waveFilter}}`;
+    const sortParams = `orderBy: variance, orderDirection: desc`;
+    const paginationParams = `first: ${first || 10}, skip: ${skip || 0}`;
+    // Query
+    const query = `{
+      contestWaveParticipants(${filterParams}, ${sortParams}, ${paginationParams}) {
+        id
+        accountAddress
+        successes
+        failures
+        variance
+      }
+    }`;
+    // Make query and return result
+    const response = await makeSubgraphQuery(query);
     return response.contestWaveParticipants;
   };
 
@@ -74,86 +132,4 @@ async function makeSubgraphQuery(query: string) {
       `could not query the subgraph: ${JSON.stringify(error.message)}`
     );
   }
-}
-
-function getFindBetsQuery(firstMember?: string, first?: number, skip?: number) {
-  let firstMemberFilter = firstMember
-    ? `firstMember: "${firstMember.toLowerCase()}"`
-    : "";
-  let filterParams = `where: {${firstMemberFilter}}`;
-  let sortParams = `orderBy: createdDate, orderDirection: desc`;
-  let paginationParams = `first: ${first || 10}, skip: ${skip || 0}`;
-  return `{
-      bets(${filterParams}, ${sortParams}, ${paginationParams}) {
-        id
-        createdDate
-        symbol
-        minPrice
-        maxPrice
-        dayStartTimestamp
-        rate
-        firstMember
-        secondMember
-        winner
-        winning
-      }
-    }`;
-}
-
-function getFindBetParticipantsQuery(
-  accountAddress?: string,
-  isCreator?: boolean,
-  isFeeForSuccess?: boolean,
-  first?: number,
-  skip?: number
-) {
-  let accountAddressFilter = accountAddress
-    ? `accountAddress: "${accountAddress.toLowerCase()}"`
-    : "";
-  let isCreatorFilter =
-    isCreator !== undefined ? `isCreator: ${isCreator}` : "";
-  let isFeeForSuccessFilter =
-    isFeeForSuccess !== undefined ? `isFeeForSuccess: ${isFeeForSuccess}` : "";
-  let filterParams = `where: {${accountAddressFilter}, ${isCreatorFilter}, ${isFeeForSuccessFilter}}`;
-  let sortParams = `orderBy: addedTimestamp, orderDirection: desc`;
-  let paginationParams = `first: ${first || 10}, skip: ${skip || 0}`;
-  return `{
-    betParticipants(${filterParams}, ${sortParams}, ${paginationParams}) {
-      id
-      bet {
-        id
-        creatorAddress
-        symbol
-        targetMinPrice
-        targetMaxPrice
-        targetTimestamp
-        feeForSuccess
-        feeForFailure
-        isClosed
-        isSuccessful
-        participantsNumber
-      }
-    }
-  }`;
-}
-
-function getFindContestWaveParticipantsQuery(
-  contestAddress: string,
-  waveIndex: number,
-  first?: number,
-  skip?: number
-) {
-  let waveFilter = `wave: "${contestAddress.toLowerCase()}_${waveIndex}"`;
-  let filterParams = `where: {${waveFilter}}`;
-  let sortParams = `orderBy: variance, orderDirection: desc`;
-  let paginationParams = `first: ${first || 10}, skip: ${skip || 0}`;
-  return `{
-    contestWaveParticipants(${filterParams}, ${sortParams}, ${paginationParams}) {
-      id
-      accountAddress
-      successes
-      failures
-      variance
-    }
-  }`;
 }
