304a83b3299bd7b358fdd9e501cfadfe9d725087
kiv1n <aloha@kiv1n.ru>
2022-12-31 15:01:40 +0300

    Display buttons to share via social networks after bet creation

--- components/bet/BetCreatedMessage.tsx
+++ components/bet/BetCreatedMessage.tsx
@@ -1,5 +1,11 @@
-import { Box, Link as MuiLink, Typography } from "@mui/material";
-import { CentralizedBox, XxlLoadingButton } from "components/styled";
+import { Telegram, Twitter } from "@mui/icons-material";
+import { Box, IconButton, Link as MuiLink, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import {
+  CentralizedBox,
+  ThickDivider,
+  XxlLoadingButton,
+} from "components/styled";
 import useToasts from "hooks/useToast";
 import Link from "next/link";
 
@@ -12,30 +18,71 @@ export default function BetPublishedMessage(props: { id: string }) {
 
   return (
     <CentralizedBox>
-      <Typography variant="h4" fontWeight={700} sx={{ mb: 3 }}>
-        🤟 Congrats, your bet is published!
+      {/* Title */}
+      <Typography variant="h4" fontWeight={700}>
+        🤟 Congrats, you made a bet!
       </Typography>
+      <ThickDivider sx={{ mt: 6 }} />
       {betLink && (
-        <>
-          {/* Link */}
-          <Box sx={{ border: 3, borderRadius: 3, px: 4, py: 2, mb: 3 }}>
-            <Link href={betLink} legacyBehavior passHref>
-              <MuiLink sx={{ fontWeight: 700, textAlign: "center" }}>
-                🔗 {betLink}
-              </MuiLink>
-            </Link>
-          </Box>
-          {/* Copy link button */}
-          <XxlLoadingButton
-            variant="contained"
-            onClick={() => {
-              navigator.clipboard.writeText(betLink);
-              showToastSuccess("Link copied");
-            }}
+        <Box sx={{ mt: 6 }}>
+          {/* Buttons to share via social networks */}
+          <Typography variant="h6" textAlign="center">
+            Share this bet via
+          </Typography>
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+            sx={{ mt: 2 }}
           >
-            Copy Link
-          </XxlLoadingButton>
-        </>
+            <IconButton
+              href={`https://twitter.com/intent/tweet?url=${betLink}`}
+              target="_blank"
+              color="primary"
+              sx={{ border: 4, p: 3 }}
+            >
+              <Twitter sx={{ fontSize: 36 }} />
+            </IconButton>
+            <IconButton
+              href={`https://t.me/share/url?url=${betLink}`}
+              target="_blank"
+              color="primary"
+              sx={{ border: 4, p: 3 }}
+            >
+              <Telegram sx={{ fontSize: 36 }} />
+            </IconButton>
+          </Stack>
+          {/* Link and copy button */}
+          <Typography variant="h6" textAlign="center" sx={{ mt: 6 }}>
+            Or copy link
+          </Typography>
+          <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                border: 3,
+                borderRadius: 5,
+                px: 4,
+              }}
+            >
+              <Link href={betLink} legacyBehavior passHref>
+                <MuiLink sx={{ fontWeight: 700, textAlign: "center" }}>
+                  🔗 {betLink}
+                </MuiLink>
+              </Link>
+            </Box>
+            <XxlLoadingButton
+              variant="contained"
+              onClick={() => {
+                navigator.clipboard.writeText(betLink);
+                showToastSuccess("Link copied");
+              }}
+            >
+              Copy
+            </XxlLoadingButton>
+          </Stack>
+        </Box>
       )}
     </CentralizedBox>
   );
